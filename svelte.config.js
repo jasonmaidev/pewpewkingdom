2f4904e328918f47f781f61299b5496a72ddb231
JFMai90 <jasonmaif@gmail.com>
2023-12-20 05:51:09 -0800

    add missing adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { imagetools } from 'vite-imagetools';
 
 /** @type {import('@sveltejs/kit').Config} */
